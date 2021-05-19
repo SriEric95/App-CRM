6093267f74b6ec6fdd8c443d7267f13882d43c83
Srivatsa <pouroucheric@gmail.com>
2021-05-19 18:34:30 +0200

    Installation de React et gestion des customers et invoices

---
+++ assets/app.js
@@ -0,0 +1,41 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+//Les imports importants
+import React from "react";
+import ReactDom from "react-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import {HashRouter, Switch, Route} from "react-router-dom";
+
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import CustomersPage from "./pages/CustomersPage";
+import CustomerPageWithPagination from "./pages/CustomerPageWithPagination";
+import InvoicesPage from "./pages/InvoicesPage";
+
+// start the Stimulus application
+//import './bootstrap';
+
+console.log("hello world !!");
+
+const App = () => {
+    return <HashRouter>
+        <Navbar />
+        <main className="container pt-5">
+            <Switch>
+                <Route path="/invoices" component={InvoicesPage} />
+                <Route path="/customers" component={CustomersPage} />
+                <Route path="/" component={HomePage} />
+            </Switch>
+        </main>
+    </HashRouter>
+};
+
+const rootElement = document.querySelector('#app');
+ReactDom.render(<App />, rootElement);
+
---
+++ assets/pages/HomePage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//Composant fonctionnel
+const HomePage = (props) => {
+    return ( 
+        <div className="jumbotron">
+            <h1 className="display-3">Hello, world !</h1>
+            <p className="lead">
+                This...
+            </p>
+            <hr className="my-4"/>
+            <p>
+                it...
+            </p>
+            <p>
+                it..
+            </p>
+            <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">
+                    Learn...
+                </a>
+            </p>
+        </div>
+     );
+}
+ 
+export default HomePage;
